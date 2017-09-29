a80f9df7c3c485eddc480790639caccef3eaf263
lisael <lisael@lisael.org>
2017-09-29 21:27:01 +0200

    added https://github.com/sos4nt/dynamic-colors as a direcrtory :/
    It's not easy to tweak without coping the whole project.
    
    TODO: fork and add as a submodule

---
+++ bin/dynamic-colors/colorschemes/gruvbox-dark.sh
@@ -0,0 +1,25 @@
+# gruvbox-dark
+
+background="#282828"
+foreground="#ebdbb2"
+cursor="#ebdbb2"
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border="#282828"
+color0="#282828" # black
+color1="#cc241d" # red
+color2="#98971a" # green
+color3="#d79921" # yellow
+color4="#458588" # blue
+color5="#b16286" # magenta
+color6="#689d6a" # cyan
+color7="#a89984" # white
+color8="#928374" # brblack
+color9="#fb4934" # brred
+color10="#b8bb26" # brgreen
+color11="#fabd2f" # bryellow
+color12="#83a598" # brblue
+color13="#d3869b" # brmagenta
+color14="#8ec07c" # brcyan
+color15="#ebdbb2" # brwhite
